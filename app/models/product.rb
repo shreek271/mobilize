6979c6f7d1d7369c4d9d2c594df1146d253c3600
shreek271 <shreenandan@rubyeffect.com>
2017-04-09 15:23:55 +0530

    added user-orders relations

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   belongs_to :brand
   belongs_to :type
   has_many :images, :dependent => :destroy
+  has_many :orders
   #accepts_nested_attributes_for :images, :allow_destroy => true
   attr_accessor :picture
 end
